2694b8dfdb902d87897964c14c02595b2a6138d2
Yan Kui Li <yankui.li@siemens.com>
2024-10-16 16:54:05 +0800

    格式化

--- data-common/src/main/java/org/example/mqtt/MqttMessageHandler.java
+++ data-common/src/main/java/org/example/mqtt/MqttMessageHandler.java
@@ -1,28 +1,28 @@
-package org.example.mqtt;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessagingException;
-import org.springframework.stereotype.Service;
-
-import java.util.Objects;
-
-/**
- * @author: xiaoliyu
- * @description: TODO
- * @dateTime: 2024/10/15 17:50
- **/
-@Service
-@Slf4j
-public class MqttMessageHandler {
-    @ServiceActivator(inputChannel = MqttSubscriberConfig.CHANNEL_NAME_IN)
-    public void handleMessage(Message<?> message) throws MessagingException {
-        String topic = Objects.requireNonNull(message.getHeaders().get("mqtt_receivedTopic")).toString();
-        String msg = message.getPayload().toString();
-        log.info("\n--------------------START-------------------\n" +
-                "接收到订阅消息:\ntopic:" + topic + "\nmessage:" + msg +
-                "\n---------------------END--------------------");
-
-    }
-}
+//package org.example.mqtt;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.integration.annotation.ServiceActivator;
+//import org.springframework.messaging.Message;
+//import org.springframework.messaging.MessagingException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Objects;
+//
+///**
+// * @author: xiaoliyu
+// * @description: TODO
+// * @dateTime: 2024/10/15 17:50
+// **/
+//@Service
+//@Slf4j
+//public class MqttMessageHandler {
+//    @ServiceActivator(inputChannel = MqttSubscriberConfig.CHANNEL_NAME_IN)
+//    public void handleMessage(Message<?> message) throws MessagingException {
+//        String topic = Objects.requireNonNull(message.getHeaders().get("mqtt_receivedTopic")).toString();
+//        String msg = message.getPayload().toString();
+//        log.info("\n--------------------START-------------------\n" +
+//                "接收到订阅消息:\ntopic:" + topic + "\nmessage:" + msg +
+//                "\n---------------------END--------------------");
+//
+//    }
+//}
